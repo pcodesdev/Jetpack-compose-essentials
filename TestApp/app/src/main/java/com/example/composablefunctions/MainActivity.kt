61184bfe3be14e443818b3524a8edc00209045df
pcodesdev <njugunapm23@gmail.com>
2023-09-05 22:10:15 +0300

    jetpack compose intro

--- TestApp/app/src/main/java/com/example/composablefunctions/MainActivity.kt
+++ TestApp/app/src/main/java/com/example/composablefunctions/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,6 +27,16 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composablefunctions.ui.theme.ComposableFunctionsTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +49,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(Message("Android","When the sun goes down!"))
+                    PreviewConversation()
+//                    Greeting(Message("Peter","When the sun goes down!"))
                 }
             }
         }
@@ -64,7 +78,12 @@ fun Greeting(msg:Message, modifier: Modifier = Modifier) {
 //    Add a horizontal space
         Spacer(modifier = Modifier.padding(24.dp))
 //    You can use Row to arrange items horizontally and Box to stack elements.
-        Column {
+//        To store this local UI state, you need to keep track of whether a message has been expanded or not. To keep track of this state change, you have to use the functions remember and mutableStateOf
+        var isExpanded by remember { mutableStateOf(false) }
+//Change the surface color from one color to another
+        val surfaceColor by animateColorAsState(if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface)
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.name,
                 color = MaterialTheme.colorScheme.secondary,
@@ -73,10 +92,21 @@ fun Greeting(msg:Message, modifier: Modifier = Modifier) {
 
 //        First, wrap the message body text around a Surface composable. Doing so allows customizing the message body's shape and elevation. Padding is also added to the message for a better layout.
             Spacer(modifier = Modifier.width(8.dp))
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+//                surface color will be changing gradually from primary to surface color
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
@@ -85,6 +115,24 @@ fun Greeting(msg:Message, modifier: Modifier = Modifier) {
 }
 
 
+@Composable
+fun Conversation(messages: List<Message>) {
+//    For this use case, use Compose’s LazyColumn and LazyRow. These composables render only the elements that are visible on screen, so they are designed to be very efficient for long lists.
+    LazyColumn {
+        items(messages) { message ->
+            Greeting(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposableFunctionsTheme {
+//        Getting data from an object
+        Conversation(SampleData.conversationSample)
+    }
+}
 
 
 //The @Preview annotation lets you preview your composable functions within Android Studio without having to build and install the app to an Android device or emulator
@@ -97,7 +145,8 @@ fun Greeting(msg:Message, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ComposableFunctionsTheme {
         Surface {
-            Greeting(Message("Android","When the sun goes down!"))
+            PreviewConversation()
+//            Greeting(Message("Android","When the sun goes down!"))
         }
 
     }
